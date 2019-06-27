267989b54190306fb58b0aa779c23468d9c35306
Diego Rodrigues <ddiegosr@hotmail.com>
2019-06-27 10:14:53 -0300

    correction in the commands to create categories table

--- app/src/main/java/com/example/danilo/appdebts/database/ScriptDLL.java
+++ app/src/main/java/com/example/danilo/appdebts/database/ScriptDLL.java
@@ -13,12 +13,12 @@ public class ScriptDLL {
     public static String createTableDebts() {
         StringBuilder sql = new StringBuilder();
         sql.append("CREATE TABLE dividas (");
-        sql.append("id INTEGER PRIMARY KEY");
-        sql.append("cod_cat INTEGER NOT NULL");
-        sql.append("valor REAL NOT NULL");
-        sql.append("descricao TEXT NOT NULL");
-        sql.append("data_vencimento DATE NOT NULL");
-        sql.append("data_pagamento DATE");
+        sql.append("id INTEGER PRIMARY KEY,");
+        sql.append("cod_cat INTEGER NOT NULL,");
+        sql.append("valor REAL NOT NULL,");
+        sql.append("descricao TEXT NOT NULL,");
+        sql.append("data_vencimento DATE NOT NULL,");
+        sql.append("data_pagamento DATE,");
         sql.append("FOREIGN KEY (cod_cat) REFERENCES categoria(id)");
         sql.append(");");
         return sql.toString();
