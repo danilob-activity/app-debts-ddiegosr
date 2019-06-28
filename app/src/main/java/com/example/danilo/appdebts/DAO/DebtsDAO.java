0e48a3df972441120c5862fd247a2470e7313c5a
Diego Rodrigues <ddiegosr@hotmail.com>
2019-06-28 07:38:38 -0300

    added method to DebtsDAO

--- app/src/main/java/com/example/danilo/appdebts/DAO/DebtsDAO.java
+++ app/src/main/java/com/example/danilo/appdebts/DAO/DebtsDAO.java
@@ -1,5 +1,90 @@
 package com.example.danilo.appdebts.DAO;
 
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.example.danilo.appdebts.classes.Category;
+import com.example.danilo.appdebts.classes.Debts;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DebtsDAO {
+    private SQLiteDatabase connection;
+    private static final String TAG = "DebtsDAO";
+
+    public DebtsDAO(SQLiteDatabase connection) {
+        this.connection = connection;
+    }
+
+    public void insert(Debts debt) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("cod_cat", debt.getCategory().getId());
+        contentValues.put("valor", debt.getValue());
+        contentValues.put("descricao", debt.getDescription());
+        contentValues.put("data_vencimento", debt.getPaymentDate());
+        contentValues.put("data_pagamento", debt.getPayDate());
+        this.connection.insertOrThrow("dividas", null, contentValues);
+        Log.d(TAG, "Divida inserida com sucesso");
+    }
+
+    public void remove(long id) {
+        String[] params = new String[1];
+        params[0] = String.valueOf(id);
+        connection.delete("dividas", "id = ?", params);
+        Log.d(TAG, "Divida ID: " + id + " excluida com sucesso");
+    }
+
+    public void alter(Debts debt) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("cod_cat", debt.getCategory().getId());
+        contentValues.put("valor", debt.getValue());
+        contentValues.put("descricao", debt.getDescription());
+        contentValues.put("data_vencimento", debt.getPaymentDate());
+        contentValues.put("data_pagamento", debt.getPayDate());
+        String[] params = new String[1];
+        params[0] = String.valueOf(debt.getId());
+        connection.update("dividas", contentValues, "id = ?", params);
+    }
+
+    public List<Debts> list() {
+        List<Debts> debts = new ArrayList<Debts>();
+        Cursor result = connection.rawQuery("SELECT * FROM dividas", null);
+        if (result.getCount() > 0) {
+            result.moveToFirst();
+            do {
+                Debts deb = new Debts();
+                deb.setId(result.getInt(result.getColumnIndexOrThrow("id")));
+                deb.setValue(result.getDouble(result.getColumnIndexOrThrow("valor")));
+                deb.setDescription(result.getString(result.getColumnIndexOrThrow("descricao")));
+                deb.setPaymentDate(result.getString(result.getColumnIndexOrThrow("data_vencimento")));
+                deb.setPayDate(result.getString(result.getColumnIndexOrThrow("data_pagamento")));
+                debts.add(deb);
+                Log.d(TAG, "Listando: " + deb.getId() + " - " + deb.getDescription());
+            } while(result.moveToNext());
+            result.close();
+        }
+        return debts;
+    }
+
+    public Debts get(long id) {
+        Debts debts = new Debts();
+        String[] params = new String[1];
+        params[0] = String.valueOf(id);
+        Cursor result = connection.rawQuery("SELECT * FROM dividas WHERE id = ?", params);
+        if(result.getCount() > 0) {
+            result.moveToFirst();
+            debts.setId(result.getInt(result.getColumnIndexOrThrow("id")));
+            debts.setValue(result.getDouble(result.getColumnIndexOrThrow("valor")));
+            debts.setDescription(result.getString(result.getColumnIndexOrThrow("descricao")));
+            debts.setPaymentDate(result.getString(result.getColumnIndexOrThrow("data_vencimento")));
+            debts.setPayDate(result.getString(result.getColumnIndexOrThrow("data_pagamento")));
+            result.close();
+            return debts;
+        }
+        return null;
+    }
 }
